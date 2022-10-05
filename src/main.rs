310f4e718367171d6e5c80bae9ac5f1385e7a273
Alexey <kulakovalexey@proton.me>
2022-10-05 07:28:34 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use crossterm::terminal;
+use rusty_audio::Audio;
+use std::io;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute();
+
+    // Cleanup
+    audio.wait();
+    Ok(())
+}
